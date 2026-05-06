4ffaed7540504eca86ba2c113e29ce581f935f46
caio-lauro <caiolaurodelima@gmail.com>
2026-05-06 14:02:59 -0300

    adicionar valores padrão para colunas relevantes

--- db/triggers.sql
+++ db/triggers.sql
@@ -4,13 +4,51 @@
 --
 
 -- Triggers para valores default
+
+-- Trigger para definir o status do usuário como ativo
 ---
 CREATE TRIGGER tg_default_usuario_ativo
 BEFORE INSERT ON usuarios
 FOR EACH ROW
 BEGIN
-    IF NEW.status is NULL THEN
+    IF NEW.status IS NULL THEN
         SET NEW.status = TRUE;
+    END IF;
+END
+---
+
+-- Trigger para definir o limite de empréstimo padrão como 5
+---
+CREATE TRIGGER tg_default_usuario_limite
+BEFORE INSERT ON usuarios
+FOR EACH ROW
+BEGIN
+    IF NEW.limite_emprestimos IS NULL THEN
+        SET NEW.limite_emprestimos = 5;
+    END IF;
+END
+---
+
+-- Trigger para definir a data de contratação padrão como a data atual
+---
+CREATE TRIGGER tg_default_funcionario_data_contratacao
+BEFORE INSERT ON funcionarios
+FOR EACH ROW
+BEGIN
+    IF NEW.data_contratacao IS NULL THEN
+        SET NEW.data_contratacao = CURRENT_DATE();
+    END IF;
+END
+---
+
+-- Trigger para definir o número de acessos padrão como 0
+---
+CREATE TRIGGER tg_default_fisico_acessos
+BEFORE INSERT ON exemplar_digital
+FOR EACH ROW
+BEGIN
+    IF NEW.numero_acessos IS NULL THEN
+        SET NEW.numero_acessos = 0;
     END IF;
 END
 ---
