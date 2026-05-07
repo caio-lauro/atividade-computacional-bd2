8cba10c4570633dfe97e6e8912a630831578f812
caio-lauro <caiolaurodelima@gmail.com>
2026-05-07 10:03:21 -0300

    adicionar procedure para adicionar automaticamente os cargos

---
+++ db/procedures.sql
@@ -0,0 +1,20 @@
+-- 
+-- ESSE ARQUIVO UTILIZA UMA SEQUÊNCIA DE TRÊS '-' 
+-- COMO SEPARADOR PARA CRIAR CADA TRIGGER.
+--
+
+-- Procedure para criar cargos automaticamente
+---
+CREATE PROCEDURE IF NOT EXISTS sp_criar_cargos ()
+BEGIN
+    INSERT IGNORE INTO cargos (cargo) VALUES
+    ('Diretor'),
+    ('Gerente'),
+    ('Assistente Administrativo'),
+    ('Bibliotecário'),
+    ('Analista Técnico'),
+    ('Documentalista'),
+    ('Recepcionista'),
+    ('Auxiliar');
+END
+---
