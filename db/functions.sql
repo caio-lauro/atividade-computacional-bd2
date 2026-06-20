10e1bec023814cfe5c9937c3a84faf222f038dfd
yCAFEEE <yuridan2705@gmail.com>
2026-06-19 23:59:31 -0300

    adicionar novas funções

---
+++ db/functions.sql
@@ -0,0 +1,10 @@
+-- Função que mostra quantos livros um usuário já pegou emprestado
+DELIMITER #
+    CREATE FUNCTION IF NOT EXISTS fn_conta_emprestimos(f_id_usuario INT) RETURNS INT
+    DETERMINISTIC
+    BEGIN
+        DECLARE cont INT;
+        SELECT COUNT(*) INTO cont FROM emprestimos WHERE f_id_usuario = id_usuario;
+        RETURN cont;
+    END #
+DELIMITER ;
