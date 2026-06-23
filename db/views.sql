8179db43e8a6d92e8d7658d6681536fdab49c3ca
caio-lauro <caiolaurodelima@gmail.com>
2026-06-23 09:57:08 -0300

    adicionar visões de livros

--- db/views.sql
+++ db/views.sql
@@ -16,3 +16,23 @@ CREATE OR REPLACE VIEW view_funcionario AS
     ON p.id = f.id_funcionario
     INNER JOIN cargos c
     ON f.id_cargo=c.id;
+
+
+-- View que contém todas as informações de um dado livro físico
+
+CREATE OR REPLACE VIEW view_fisico AS
+    SELECT l.id, l.ISBN, l.titulo, l.data_publicacao, 
+    f.disponivel, e.identificador_fisico Estante
+    FROM livros l INNER JOIN exemplar_fisico f
+    ON l.id=f.id_fisico 
+    INNER JOIN estantes e
+    ON f.id_estante_associada=e.id;
+
+
+-- View que contém todas as informações de um dado livro digital
+
+CREATE OR REPLACE VIEW view_fisico AS
+    SELECT l.id, l.ISBN, l.titulo, l.data_publicacao, 
+    d.numero_acessos, d.URL
+    FROM livros l INNER JOIN exemplar_digital d
+    ON l.id=d.id_digital;
