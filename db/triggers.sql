cf4a3169315cd1d9868ba0cb2989e08813ef268b
caio-lauro <caiolaurodelima@gmail.com>
2026-06-23 14:14:40 -0300

    substituir lógica de transaction por sqlstate

--- db/triggers.sql
+++ db/triggers.sql
@@ -59,31 +59,30 @@ CREATE TRIGGER IF NOT EXISTS tg_emprestimo
 BEFORE INSERT ON emprestimos
 FOR EACH ROW
 BEGIN
-    SET autocommit = 0;
     DECLARE empresitmos_ativos INT;
     DECLARE t_limite_emprestimos INT;
     DECLARE disponibilidade_livro BOOLEAN;
 
-    START TRANSACTION;
     SELECT fn_conta_emprestimos_ativos(NEW.id_usuario) INTO empresitmos_ativos;
-    SELECT limite_emprestimos INTO tg_limite_emprestimos FROM usuarios WHERE id_usuario = NEW.id_usuario;
+    SELECT limite_emprestimos INTO t_limite_emprestimos FROM usuarios WHERE id_usuario = NEW.id_usuario;
     SELECT disponivel INTO disponibilidade_livro FROM exemplar_fisico WHERE id_fisico = NEW.id_fisico;
 
     -- Vefifica se o livro não está disponível
     IF disponibilidade_livro = FALSE THEN
-        ROLLBACK;
+        SIGNAL SQLSTATE '40000'
+            SET MESSAGE_TEXT='Livro indisponível para empréstimo.';
     END IF;
 
     -- verificar se o usuario pode fazer o emprestimo
     IF empresitmos_ativos >= t_limite_emprestimos THEN
-        ROLLBACK;
-    ELSE THEN
-        -- colocar disponivel no exemplar fisico como false
-        UPDATE exemplar_fisico SET disponivel = FALSE WHERE id_fisico = NEW.id_fisico;
+        SIGNAL SQLSTATE '40000' 
+            SET MESSAGE_TEXT='Não é possível realizar mais empréstimos para esse usuário.';
     END IF;
+
+    -- colocar disponivel no exemplar fisico como false
+    UPDATE exemplar_fisico SET disponivel = FALSE WHERE id_fisico = NEW.id_fisico;
 
     SET NEW.data_emprestimo = CURDATE();
     SET NEW.data_devolucao = DATE_ADD(CURDATE(), INTERVAL 20 DAY);
-    COMMIT;
 END
 ---
