ca1b28cfb97393249b319da2a479eac50b768872
yCAFEEE <yuridan2705@gmail.com>
2026-06-23 09:50:03 -0300

    add função que conta emprestimos ativos

--- db/functions.sql
+++ db/functions.sql
@@ -6,5 +6,16 @@ BEGIN
     DECLARE cont INT;
     SELECT COUNT(*) INTO cont FROM emprestimos WHERE f_id_usuario = id_usuario;
     RETURN cont;
-END #
+END 
+---
+
+-- Função que retorna a quantidade de emprestimos ativos de um usuário
+---
+CREATE FUNCTION IF NOT EXISTS fn_conta_emprestimos_ativos(f_id_usuario INT)
+DETERMINISTIC
+BEGIN
+    DECLARE cont_ativos INT;
+    SELECT COUNT(*) INTO cont_ativos FROM empresitmos WHERE f_id_usuario = id_usuario AND devolvido = FALSE;
+    RETURN cont_ativos;
+END 
 ---
